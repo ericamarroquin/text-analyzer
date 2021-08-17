2011a4ffa13323166b51f16856b5b53158b1c6f7
EricaMarroquin <ericamarroquin03@gmail.com>
2021-08-17 09:54:16 -0700

    add tests for wordCounter

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,15 @@
 // Business Logic
 
 function wordCounter(text) {
+  if (text.trim().length === 0) {
+    return 0;
+  }
   let wordCount = 0;
   const wordArray = text.split(" ");
-  wordArray.forEach(function(word) {
-    wordCount++;
+  wordArray.forEach(function(element) {
+    if (!Number(element)) {
+      wordCount++;
+    }
   });
   return wordCount;
 }

464531e6377794036ed06088cb9835d4dfd37827
EricaMarroquin <ericamarroquin03@gmail.com>
2021-08-17 11:36:20 -0700

    add helper function to DRY code

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+// Utility Logic
+
+function noInputtedWord(word, text) {
+  return ((text.trim().length === 0 || word.trim().length === 0));
+}
+
 
 // Business Logic
 
@@ -16,7 +22,7 @@ function wordCounter(text) {
 }
 
 function numberOfOccurrencesInText(word, text) {
-  if (text.trim().length === 0 || (word.trim().length ===0)) {
+  if (noInputtedWord(word, text)) {
     return 0;
   }
   const wordArray = text.split(" ");
@@ -29,7 +35,12 @@ function numberOfOccurrencesInText(word, text) {
   return wordCount;
 }
 
+// UI Logic
+
 function boldPassage(word,text) {
+  if (noInputtedWord(word, text)) {
+    return "";
+  }
   let htmlString = "<p>";
   let textArray = text.split(" ");
   textArray.forEach(function(element, index) {
@@ -45,8 +56,6 @@ function boldPassage(word,text) {
   return htmlString + "</p>";
 }
 
-// UI Logic
-
 $(document).ready(function() {
   $("form#word-counter").submit(function(event) {
     event.preventDefault();
@@ -56,5 +65,6 @@ $(document).ready(function() {
     const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
     $("#total-count").html(wordCount);
     $("#selected-count").html(occurrencesOfWord);
+    $("#bolded-passage").html(boldPassage(word,passage));
   });
 });
